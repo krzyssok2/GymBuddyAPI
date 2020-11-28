aecafb9d49da619edd582b5d37924ee93701d305
Kšyštof Stanislav Sokolovski <sokolkrzys1999@gmail.com>
2020-11-28 15:10:52 +0200

    Connected to database

---
+++ GymBuddy/GymBuddyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymBuddyAPI
+{
+    public class GymBuddyContext : IdentityDbContext<IdentityUser>
+    {
+        public GymBuddyContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            //builder.ApplyConfiguration(new CitiesConfiguration());
+        }
+    }
+}
--- GymBuddy/Startup.cs
+++ GymBuddy/Startup.cs
@@ -5,14 +5,20 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using GymBuddyAPI;
+using GymBuddyAPI.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.Filters;
 
@@ -30,6 +36,36 @@ namespace GymBuddy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GymBuddyContext>(options => options.UseSqlServer(Configuration.GetConnectionString("GymBuddyAPI")));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<GymBuddyContext>().AddDefaultTokenProviders();
+
+            var jwtSettings = new JwtSettings();
+            Configuration.Bind(nameof(jwtSettings), jwtSettings);
+            services.AddSingleton(jwtSettings);
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                RequireExpirationTime = false,
+                ValidateLifetime = true
+            };
+
+            services.AddSingleton(tokenValidationParameters);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = tokenValidationParameters;
+            });
+
             services.AddControllers();
 
             services.AddSwaggerGen(x =>
@@ -37,7 +73,31 @@ namespace GymBuddy
                 x.SwaggerDoc("v1", new OpenApiInfo { Title = "GymBuddy API", Version = "v1" });
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-               // x.IncludeXmlComments(xmlPath);
+                // x.IncludeXmlComments(xmlPath);
+
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer", new string[0] }
+                };
+
+                var scheme = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the bearer scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    Reference = new OpenApiReference
+                    {
+                        Id = JwtBearerDefaults.AuthenticationScheme,
+                        Type = ReferenceType.SecurityScheme
+                    }
+                };
+
+                x.AddSecurityDefinition(name: "Bearer", scheme);
+
+                x.OperationFilter<SecurityRequirementsOperationFilter>(true, scheme.Reference.Id);
             });
         }
 
